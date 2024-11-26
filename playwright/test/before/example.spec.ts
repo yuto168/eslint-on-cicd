3fb04f24a37c0a1ea9940fb5f36580756a87f431
yuto168 <kanawo202090@gmail.com>
2024-11-27 08:19:44 +0900

    delete 不要なテスト

--- playwright/test/before/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
-
-test('スクショを撮る', async ({ page }) => {
-  await page.screenshot({ path: './vrt/before/example.png', fullPage: true });
-});
